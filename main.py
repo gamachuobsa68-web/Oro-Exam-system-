c5c045896856a26501f3e25c0a2b55cb70e2c9c0
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-03 22:33:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,21 @@
 from kivy.app import App
-from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
 
 from screens.login import LoginScreen
 from screens.dashboard import DashboardScreen
-from screens.admin import AdminScreen
-from screens.teacher import TeacherScreen
-from screens.student import StudentScreen
 from screens.exam import ExamScreen
+from screens.leaderboard import LeaderboardScreen
 
 
 class ExamApp(App):
     def build(self):
-        Builder.load_file("main.kv")
-
         sm = ScreenManager()
 
         sm.add_widget(LoginScreen(name="login"))
         sm.add_widget(DashboardScreen(name="dashboard"))
-        sm.add_widget(AdminScreen(name="admin"))
-        sm.add_widget(TeacherScreen(name="teacher"))
-        sm.add_widget(StudentScreen(name="student"))
         sm.add_widget(ExamScreen(name="exam"))
+        sm.add_widget(LeaderboardScreen(name="leaderboard"))
 
-        sm.current = "login"
         return sm
 
 
