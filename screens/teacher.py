68a647b1022135ace6c5541e54a7c23aba3f367d
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-04 08:32:55 +0300

    Update teacher.py

--- screens/teacher.py
+++ screens/teacher.py
@@ -1,4 +1,12 @@
 from kivy.uix.screenmanager import Screen
+from kivy.uix.label import Label
+
 
 class TeacherScreen(Screen):
-    pass
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.add_widget(
+            Label(text="Teacher Dashboard")
+        )
