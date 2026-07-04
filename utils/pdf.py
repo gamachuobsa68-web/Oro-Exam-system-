9f4dd2b7ee67ddac4e8ccbac720b36e7ff29d513
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-04 09:01:29 +0300

    Update pdf.py

--- utils/pdf.py
+++ utils/pdf.py
@@ -1,14 +1,28 @@
-from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
 
-def export_pdf(student, score, total):
-    file = "result.pdf"
-    c = canvas.Canvas(file, pagesize=letter)
 
-    c.drawString(100, 750, "ORO EXAM RESULT")
-    c.drawString(100, 700, f"Student: {student}")
-    c.drawString(100, 680, f"Score: {score}/{total}")
+def generate_result_pdf(student_name, score, total):
+
+    file_name = f"{student_name}_result.pdf"
+
+    c = canvas.Canvas(file_name, pagesize=letter)
+
+    c.setFont("Helvetica-Bold", 20)
+    c.drawString(180, 750, "ORO EXAM RESULT")
+
+    c.setFont("Helvetica", 14)
+    c.drawString(100, 700, f"Student Name: {student_name}")
+    c.drawString(100, 670, f"Score: {score} / {total}")
+    c.drawString(100, 640, f"Percentage: {round((score/total)*100, 2)} %")
+
+    if score >= total * 0.5:
+        status = "PASS"
+    else:
+        status = "FAIL"
+
+    c.drawString(100, 610, f"Status: {status}")
 
     c.save()
 
-    return file
+    return file_name
