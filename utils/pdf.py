55aa73736d236ef15367b2e48b4834741bda4921
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-04 09:23:18 +0300

    Update pdf.py

--- utils/pdf.py
+++ utils/pdf.py
@@ -7,12 +7,16 @@ def generate_result_pdf(student, score, total):
 
     c = canvas.Canvas(file)
 
-    c.drawString(100, 750, "ORO EXAM RESULT")
-    c.drawString(100, 700, f"Student: {student}")
-    c.drawString(100, 680, f"Score: {score}/{total}")
+    c.drawString(200, 800, "ORO EXAM RESULT")
+
+    c.drawString(100, 750, f"Student: {student}")
+    c.drawString(100, 730, f"Score: {score}/{total}")
 
     percent = (score / total) * 100
-    c.drawString(100, 660, f"Percentage: {percent}%")
+    c.drawString(100, 710, f"Percentage: {percent}%")
+
+    status = "PASS" if score >= total/2 else "FAIL"
+    c.drawString(100, 690, f"Status: {status}")
 
     c.save()
 
