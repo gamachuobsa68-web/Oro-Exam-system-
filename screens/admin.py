6df132d6e191284e5acff787f20d660eed273717
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-04 08:33:48 +0300

    Update admin.py

--- screens/admin.py
+++ screens/admin.py
@@ -1,4 +1,12 @@
 from kivy.uix.screenmanager import Screen
+from kivy.uix.label import Label
+
 
 class AdminScreen(Screen):
-    pass
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.add_widget(
+            Label(text="Admin Dashboard")
+        )
