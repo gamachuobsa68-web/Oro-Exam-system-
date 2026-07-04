0f1a75aff5937f947850884ef1622cbea583d308
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-04 08:36:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,32 @@ from kivy.uix.screenmanager import ScreenManager
 
 from screens.login import LoginScreen
 from screens.dashboard import DashboardScreen
+from screens.teacher import TeacherScreen
+from screens.admin import AdminScreen
+from screens.exam import ExamScreen
+from screens.leaderboard import LeaderboardScreen
+
+
+class WindowManager(ScreenManager):
+    pass
+
 
 class OroExamSystem(App):
 
     def build(self):
-
-        sm = ScreenManager()
+        sm = WindowManager()
 
         sm.add_widget(LoginScreen(name="login"))
         sm.add_widget(DashboardScreen(name="dashboard"))
+        sm.add_widget(TeacherScreen(name="teacher"))
+        sm.add_widget(AdminScreen(name="admin"))
+        sm.add_widget(ExamScreen(name="exam"))
+        sm.add_widget(LeaderboardScreen(name="leaderboard"))
+
+        sm.current = "login"
 
         return sm
+
 
 if __name__ == "__main__":
     OroExamSystem().run()
