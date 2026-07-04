eef12888e1e68f7b5c5bae7f68996581d878cfe1
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-04 08:31:10 +0300

    Update login.py

--- screens/login.py
+++ screens/login.py
@@ -1,24 +1,62 @@
 from kivy.uix.screenmanager import Screen
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.spinner import Spinner
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 
+from core.auth import login
+
 
 class LoginScreen(Screen):
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        layout = BoxLayout(orientation="vertical", padding=20, spacing=10)
+        layout = BoxLayout(
+            orientation="vertical",
+            padding=20,
+            spacing=10
+        )
 
-        self.user = TextInput(hint_text="Student ID")
+        self.role = Spinner(
+            text="student",
+            values=("student", "teacher", "admin")
+        )
+
+        self.username = TextInput(
+            hint_text="Username"
+        )
+
+        self.password = TextInput(
+            hint_text="Password",
+            password=True
+        )
+
         btn = Button(text="Login")
+        btn.bind(on_press=self.check_login)
 
-        btn.bind(on_press=self.go)
-
-        layout.add_widget(self.user)
+        layout.add_widget(self.role)
+        layout.add_widget(self.username)
+        layout.add_widget(self.password)
         layout.add_widget(btn)
 
         self.add_widget(layout)
 
-    def go(self, instance):
-        self.manager.current = "dashboard"
+    def check_login(self, instance):
+
+        ok = login(
+            self.role.text,
+            self.username.text,
+            self.password.text
+        )
+
+        if ok:
+
+            if self.role.text == "student":
+                self.manager.current = "dashboard"
+
+            elif self.role.text == "teacher":
+                self.manager.current = "teacher"
+
+            else:
+                self.manager.current = "admin"
