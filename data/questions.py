23016d58d2d4013877fb0bd3b79464700c49b0ce
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-04 09:00:50 +0300

    Update questions.py

--- data/questions.py
+++ data/questions.py
@@ -1,17 +1,24 @@
+# 🎓 ORO EXAM QUESTIONS
+
 QUESTIONS = [
     {
-        "q": "2 + 2 = ?",
-        "options": ["1", "2", "4", "5"],
+        "question": "What is 2 + 2?",
+        "options": ["2", "3", "4", "5"],
         "answer": "4"
     },
     {
-        "q": "5 * 3 = ?",
-        "options": ["10", "15", "20", "25"],
-        "answer": "15"
+        "question": "Capital of Ethiopia?",
+        "options": ["Addis Ababa", "Nairobi", "Cairo", "London"],
+        "answer": "Addis Ababa"
     },
     {
-        "q": "10 / 2 = ?",
-        "options": ["2", "5", "6", "8"],
-        "answer": "5"
+        "question": "Which is a programming language?",
+        "options": ["HTML", "CSS", "Python", "Photoshop"],
+        "answer": "Python"
+    },
+    {
+        "question": "5 x 5 = ?",
+        "options": ["10", "20", "25", "30"],
+        "answer": "25"
     }
 ]
