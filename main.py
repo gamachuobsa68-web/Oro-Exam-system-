1c1e409f77494181bc7bada1950a24d5f28e7492
gamachuobsa68-web <gamachuobsa68@gmail.com>
2026-07-05 11:06:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager
+
 from core.db import init_db
+
+from screens.login import LoginScreen
+from screens.dashboard import DashboardScreen
+from screens.teacher import TeacherScreen
+from screens.admin import AdminScreen
+from screens.exam import ExamScreen
+from screens.leaderboard import LeaderboardScreen
 
 
 class AppMain(App):
 
     def build(self):
 
-        init_db()   # 🔥 IMPORTANT
+        init_db()
 
         sm = ScreenManager()
 
@@ -19,3 +29,7 @@ class AppMain(App):
         sm.current = "login"
 
         return sm
+
+
+if __name__ == "__main__":
+    AppMain().run()
